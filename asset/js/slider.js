7a97470e50535aa39913a87be0c9a38032eea4ca
unknown <sonhong443@gmail.com>
2022-05-11 09:17:52 +0700

    Done 100% slider

--- asset/js/slider.js
+++ asset/js/slider.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function() {
 
+    // Get elements
     const slider = $('.slider')
     const sliderMain = $('.slider__main')
     const sliderItem = $$('.slider__item')
@@ -7,24 +8,29 @@ window.addEventListener('load', function() {
     const sliderPrevBtn = $('.slider__prev-btn')
     const sliderNextBtn = $('.slider__next-btn')
 
-    const sliderDotItem = $$('.slider__dot-item')
+    let sliderDotsItem = $$('.slider__dot-item')
 
+    // Create default value of slide
     let indexItem = 0 //as Item 0
     let xItem = `${indexItem}00%`
     const slideItemLength = sliderItem.length
 
-    // Create function handling click prev or next slide
+    // -----------------------------------------------------------
+
+    // Create function handling click prev, next slide or dot item
     const handleClickToggleSlide = (direction) => {
+        // Next
         if (direction === 1) {
 
             indexItem--
             if (indexItem === -slideItemLength) {
-                indexItem = 0
+                indexItem = -0
             }
             xItem = `${indexItem}00%`
 
             sliderMain.style = `transform: translateX(${xItem})`
-        } else {
+        // Prev
+        } else if (direction === -1)  {
 
             indexItem++
             if (indexItem === 1) {
@@ -33,15 +39,53 @@ window.addEventListener('load', function() {
             xItem = `${indexItem}00%`
 
             sliderMain.style = `transform: translateX(${xItem})`
+        // Dot item
+        } else if (typeof direction == 'object') {
+            
+            indexItem = -direction.indexDotItem
 
+            xItem = '-' + direction.xDotItem
+
+            sliderMain.style = `transform: translateX(${xItem})`
         }
+
+        console.log(indexItem)
+        // Handle dot item in UI
+        Array(sliderDotsItem)[0].forEach((dotItem) => {
+            if (dotItem.matches('.active') && -dotItem.dataset.index != indexItem) {
+                dotItem.classList.remove('active')
+            } else if (-dotItem.dataset.index == indexItem) {
+                dotItem.classList.add('active')
+            }
+        })
     }
 
+    // -----------------------------------------------------------
+    // Handle click prev btn
     sliderPrevBtn.addEventListener('click', function() {
         handleClickToggleSlide(-1)
     })
+
+    // Handle click next btn
     sliderNextBtn.addEventListener('click', function() {
         handleClickToggleSlide(1)
     })
+
+    // Handle click dot item
+    Array(sliderDotsItem)[0].forEach((dotItem) => {
+        dotItem.addEventListener('click', function() {
+
+            handleClickToggleSlide({
+                xDotItem: `${dotItem.dataset.index}00%`,
+                indexDotItem: Number.parseInt(dotItem.dataset.index)
+            })
+
+        })
+    })
+
+    // -----------------------------------------------------------
+    setInterval(() => {
+        sliderNextBtn.click()
+    }, 5000)
 })
 
